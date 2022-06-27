90012a56541bc945bf70d3639163c8be15375e7e
junengt <95681705+junengt@users.noreply.github.com>
2022-06-27 15:00:48 +0900

    Initial commit

---
+++ src/main/java/algorithm/javaalgorithm/JavaAlgorithmApplication.java
@@ -0,0 +1,29 @@
+package algorithm.javaalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JavaAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAlgorithmApplication.class, args);
+
+        /**
+         * FindString
+         */
+//        FindString findString = new FindString();
+//
+//            Scanner sc = new Scanner(System.in);
+//
+//            String str = sc.next();
+//            char c = sc.next().charAt(0);
+//
+//            System.out.println(findString.solution(str, c));
+
+
+
+    }
+
+}
---
+++ src/test/java/algorithm/javaalgorithm/JavaAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package algorithm.javaalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
